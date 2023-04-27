cb2b36cba9afc030f7a8e1126aa20fee2a989638
dsuekamling <dsuekamling@gmail.com>
2023-04-27 15:11:11 -0400

    update

--- src/App.js
+++ src/App.js
@@ -8,31 +8,56 @@ import Login from './components/pages/client/login/Login';
 import Adminforum from './components/pages/admin/admin-forum/Adminforum';
 import Adminhome from './components/pages/admin/admin-home/Adminhome';
 import AccountCreation from './components/pages/admin/admin-account/AccountCreation';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import {useState} from 'react';
+import ClientNavbar from './components/bars/client-navbar/ClientNavbar';
+import Footer from './components/bars/foot/footer';
+import UserCalendar from './components/pages/client/calendar/UserCalendar';
+import AdminCalendar from './components/pages/admin/admin-calendar/AdminCalendar';
 
 function App() {
-  return (
-    <div className= 'App'>
-    <BrowserRouter>
-    <Routes>
-    <Route path= '/' element={<Casa />} />
-    <Route path= '/about' element={<About />} />
-    <Route path= '/meet' element={<Meet />} />
-    <Route path= '/forum' element={<Forum />} />
-    <Route path= '/contact' element={<Contact />} />
-    <Route path= '/login' element={<Login />} />
-    
-    </Routes>
-    </BrowserRouter>
-  </div>
+  const [role, setRole] = useState("");
 
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ClientNavbar />
+
+        <Routes>
+          <Route path="/" element={<Casa />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/meet" element={<Meet />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {role === "user" && (
+            <>
+              <Route path="/forum" element={<Forum />} />
+              <Route path="/calendar" element={<UserCalendar />} />
+            </>
+          )}
+
+          {role === "admin" && (
+            <>
+              <Route path="/admin/forum" element={<Adminforum />} />
+              <Route path="/admin/home" element={<Adminhome />} />
+              <Route path="/admin/account-creation" element={<AccountCreation />} />
+              <Route path="/admin/calendar" element={<AdminCalendar />} />
+            </>
+          )}
+
+          {!role && (
+            <>
+              <Route path="/forum" element={<Forum />} />
+              <Route path="/calendar" element={<UserCalendar />} />
+              <Route path="/login" element={<Login setRole={setRole} />} />
+            </>
+          )}
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
-
-
-
-
