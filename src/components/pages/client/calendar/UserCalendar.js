7b057334fa668b93f5d3a4eeb7d7156c76035f65
dsuekamling <dsuekamling@gmail.com>
2023-04-27 15:20:52 -0400

    update

---
+++ src/components/pages/client/calendar/UserCalendar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+const UserCalendar = ({ events }) => {
+  const handleEventClick = (clickInfo) => {
+    alert(clickInfo.event.title);
+  };
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventClick={handleEventClick}
+    />
+  );
+};
+
+export default UserCalendar;
+
+  
