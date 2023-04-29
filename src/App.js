e35c251fa146db9091c1499b73b898f15d5beccb
CtrAltElite <abarrerasprogrammer@gmail.com>
2023-04-29 13:53:24 -0400

    Fixed issues with locally hosted images for the footer and the meet the team page.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Adminforum from './components/pages/admin/admin-forum/Adminforum';
 import Adminhome from './components/pages/admin/admin-home/Adminhome';
 import AccountCreation from './components/pages/admin/admin-account/AccountCreation';
 import AdminAbout from './components/pages/admin/admin-about/AdminAbout';
+import DiscSymph from './components/pages/client/forum/discussion/DiscSymph/DiscSymph';
 
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -29,6 +30,7 @@ function App() {
     <Route path= '/Adminhome' element={<Adminhome />} />
     <Route path= '/Adminforum' element={<Adminforum />} />
     <Route path= '/AdminAbout' element={<AdminAbout />} />
+    <Route path= '/DiscSymph' element={<DiscSymph />} />
 
 
     
